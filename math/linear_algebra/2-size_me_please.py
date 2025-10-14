02bd4c5bcad097b75e3cc5c92d99fa9ee94980ae
saralozi <saralozi08@gmail.com>
2025-10-14 16:08:46 +0000

    Calculating shape of a matrix

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""A script that calculates the shape of a matrix"""
+
+
+def matrix_shape(matrix):
+    """Function that calculates the shape of a matrix"""
+    matrix_shape = []
+
+    while type(matrix) is list:
+        matrix_shape.append(len(matrix))
+        matrix = matrix[0]
+
+    return matrix_shape

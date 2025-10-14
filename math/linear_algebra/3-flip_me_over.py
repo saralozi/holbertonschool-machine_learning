76c678942fe61afc9a1ce541fb457e7ebd7bd3f9
saralozi <saralozi08@gmail.com>
2025-10-14 16:53:33 +0000

    Adding task 3

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def matrix_transpose(matrix):
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+    transposed = [[0] * rows for i in range(cols)]
+
+    for i in range(rows):
+        for j in range(cols):
+            transposed[j][i] = matrix[i][j]
+
+    return transposed

9c44ca469410b2dc6b859a13b2a815f9b5c8e797
saralozi <saralozi08@gmail.com>
2025-10-17 18:58:21 +0200

    Adding solution for task 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A script that concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """Function that concatenates two arrays"""
+    arr3 = []
+    arr3 = arr1 + arr2
+    return arr3

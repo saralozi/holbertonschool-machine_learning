68ddbb83d9c53f26f67ee70dde0d6d4b29ba2645
saralozi <saralozi08@gmail.com>
2025-10-17 18:18:01 +0200

    Adding solution for task 5

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""A script that adds two arrays element-wise"""
+
+
+def add_matrices2D(mat1, mat2):
+    """Function that adds two 2D matrices element-wise"""
+    if (len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0])):
+        mat3 = []
+
+        for i in range(len(mat1)):
+            row = []
+            for j in range(len(mat1[0])):
+                row.append(mat1[i][j] + mat2[i][j])
+            mat3.append(row)
+        return mat3
+    else:
+        return None

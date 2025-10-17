ed31743a3dd17b4930e9fd16dd3d37d9933de733
saralozi <saralozi08@gmail.com>
2025-10-17 16:14:27 +0200

    Adding documentation for task 3

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+"""A script that returns the transpose of a 2D matrix"""
+
 
 def matrix_transpose(matrix):
-
+    """Function that returns the transpose of a 2D matrix"""
     rows = len(matrix)
     cols = len(matrix[0])
     transposed = [[0] * rows for i in range(cols)]

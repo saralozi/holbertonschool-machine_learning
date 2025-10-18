6d5ea4774492c1c4cc0b879f57f430bf74480525
saralozi <saralozi08@gmail.com>
2025-10-18 18:46:10 +0200

    Adding solution for task 12

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A script that performs element-wise operations"""
+
+
+def np_elementwise(mat1, mat2):
+    """A function that performs element-wise operations"""
+    return (mat1+mat2, mat1-mat2, mat1*mat2, mat1/mat2)

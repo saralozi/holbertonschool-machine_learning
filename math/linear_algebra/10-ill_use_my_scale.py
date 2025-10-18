aa086acd26586c240e39c8dbc762fa2c5d4da03f
saralozi <saralozi08@gmail.com>
2025-10-18 16:35:20 +0200

    Adding the solution for task 10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A script that calculates the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """A function that calculates the shape of a numpy.ndarray"""
+    result = {}
+    result = matrix.shape
+
+    return result

84e001a4d181ec546f57dfd8e14224c0dc2aff1a
saralozi <saralozi08@gmail.com>
2025-10-18 17:36:50 +0200

    Addong solution for task 11

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """A script that transposes matrix"""
-import numpy as np
 
 
 def np_transpose(matrix):
     """A function that returns the transpose of a matrix"""
-    return np.transpose(matrix)
+    return matrix.T

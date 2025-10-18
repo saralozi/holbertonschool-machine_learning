587351b7d341952969f5fbd37de7ebb859d11ef8
saralozi <saralozi08@gmail.com>
2025-10-18 18:58:11 +0200

    Adding solution for task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""A script that performs matrix multiplication"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """A function that performs matrix multiplication"""
+    return np.dot(mat1, mat2)

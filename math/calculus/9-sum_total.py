229c26e43b9f763a2b0cf178509cdf9bbafd235f
saralozi <saralozi08@gmail.com>
2025-10-21 13:18:03 +0200

    Addint solution for task 9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""A script that calculates the sum of powers of n"""
+
+
+def summation_i_squared(n):
+    """A function that calculates the sum of powers of n"""
+    if type(n) is not int:
+        return None
+    else:
+        return int((n*(n+1)*(2*n+1))/6)

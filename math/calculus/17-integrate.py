c094992293ac607d8239dd5eff63f26113962e50
saralozi <saralozi08@gmail.com>
2025-10-22 00:32:24 +0200

    Adding solution for task 17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,3 +4,12 @@
 
 def poly_integral(poly, C=0):
     """A function that calculates the integral of a polynomial"""
+    if not isinstance(poly, list) or len(poly) == 0 or not isinstance(C, int):
+        return None
+
+    integral = [C]
+    for i in range(len(poly)):
+        res = poly[i] / (i + 1)
+        res = int(res) if res.is_integer() else res
+        integral.append(res)
+    return integral

d531e388d2a54fb8952425648c7b8d21eab42614
saralozi <saralozi08@gmail.com>
2025-10-22 00:03:10 +0200

    Adding solution for task 10

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""A script that calculates the integral of a polynomial"""
+
+
+def poly_integral(poly, C=0):
+    """A function that calculates the integral of a polynomial"""

36a07a1691ed920cb672e9a4d9ddfc5bcbca8100
saralozi <saralozi08@gmail.com>
2025-10-24 18:40:38 +0200

    Adding solution for task 6

---
+++ math/probability/normal.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""A script that represents a normal distribution"""
+
+
+class Normal:
+    """Normal distribution"""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+            return
+
+        if not isinstance(data, list):
+            raise TypeError("data must be a list")
+        if len(data) < 2:
+            raise ValueError("data must contain multiple values")
+
+        mean = sum(data) / len(data)
+        stddev = (sum((mean - num) ** 2 for num in data) / len(data)) ** 0.5
+
+        self.mean = float(mean)
+        self.stddev = float(stddev)

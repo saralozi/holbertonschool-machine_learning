f3588234aee99fdf7634f6a1c8dd2a5e70eaf282
saralozi <saralozi08@gmail.com>
2025-10-31 02:43:27 +0100

    Adding solution for task 1

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -5,10 +5,8 @@
 import pandas as pd
 
 
-df = pd.DataFrame(
-    {
+data = {
         "First": [0.0, 0.5, 1.0, 1.5],
-        "Second": ["one", "two", "three", "four"]
-    },
-    index=["A", "B", "C", "D"]
-)
+        "Second": ["one", "two", "three", "four"]}
+
+df = pd.DataFrame(data, index=["A", "B", "C", "D"])

16575f5fcf5bde53cd162702db3e52be8928ea48
saralozi <saralozi08@gmail.com>
2025-11-04 00:32:15 +0100

    Adding solution for task 3

---
+++ math/plotting/3-two.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""A script that plots x ↦ y1 and x ↦ y2 as line graphs"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def two():
+    """A function that plots 2 line graphs"""
+
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.plot(x, y1, color='red', linestyle='dashed', label="C-14")
+    plt.plot(x, y2, color='green', label="Ra-226")
+
+    plt.title("Exponential Decay of Radioactive Elements")
+    plt.xlabel("Time (years)")
+    plt.ylabel("Fraction Remaining")
+
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+
+    plt.legend()
+    plt.show()

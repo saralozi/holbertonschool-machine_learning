0cfa90ac2482c9c8b923af9c79024fdd220d0a9b
saralozi <saralozi08@gmail.com>
2025-11-04 15:37:16 +0100

    Fixing solution for task 6

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -17,41 +17,25 @@ def bars():
     x = np.arange(fruit.shape[1])
     width = 0.5
 
-    plt.bar(
-        x,
-        fruit[0],
-        width,
-        label='apples',
-        color='red')
-    plt.bar(
-        x,
-        fruit[1],
-        width,
-        bottom=fruit[0],
-        label='bananas',
-        color='yellow')
-    plt.bar(
-        x,
-        fruit[2],
-        width,
-        bottom=fruit[0] + fruit[1],
-        label='oranges',
-        color='#ff8000',
-    )
-    plt.bar(
-        x,
-        fruit[3],
-        width,
-        bottom=fruit[0] + fruit[1] + fruit[2],
-        label='peaches',
-        color='#ffe5b4',
-    )
+    apples = fruit[0]
+    bananas = fruit[1]
+    oranges = fruit[2]
+    peaches = fruit[3]
+
+    plt.bar(x, apples, width, label='apples', color='red', edgecolor='none')
+    plt.bar(x, bananas, width, bottom=apples, label='bananas',
+            color='yellow', edgecolor='none')
+    plt.bar(x, oranges, width, bottom=apples + bananas, label='oranges',
+            color='#ff8000', edgecolor='none')
+    plt.bar(x, peaches, width, bottom=apples + bananas + oranges,
+            label='peaches', color='#ffe5b4', edgecolor='none')
 
     plt.xticks(x, people)
     plt.ylabel('Quantity of Fruit')
     plt.yticks(np.arange(0, 81, 10))
     plt.ylim(0, 80)
     plt.title('Number of Fruit per Person')
-    plt.legend()
-    plt.tight_layout()
+
+    plt.legend(loc='upper right', frameon=True)
+
     plt.show()

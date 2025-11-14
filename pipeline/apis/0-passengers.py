69f23dd9aac95a7b821713e67d04fb1d0ec53941
saralozi <saralozi08@gmail.com>
2025-11-15 00:16:43 +0100

    Adding solution for task 0

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""A script that returns the list of ships that
+can hold a given number of passengers"""
+
+
+import requests
+import time
+
+
+def availableShips(passengerCount):
+    """A function that returns the list of ships than
+    can hold a given number of passengers"""
+
+    url = "https://swapi-api.hbtn.io/api/starships/"
+    ships = []
+
+    while url:
+        response = requests.get(url, timeout=10)
+
+        if response.status_code != 200:
+            print("Error fetching data")
+            return []
+
+        else:
+            data = response.json()
+
+            for ship in data['results']:
+                passengers = ship['passengers']
+
+                if not passengers.isdigit():
+                    continue
+
+                if int(passengers) >= passengerCount:
+                    ships.append(ship['name'])
+
+            time.sleep(1)
+            url = data.get('next')
+
+    return ships
---
+++ pipeline/apis/README.md
@@ -0,0 +1,1 @@
+# APIs in Machine Learning

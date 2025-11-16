dea84eae5e96107d43067ef7f2e17355fc574b74
saralozi <saralozi08@gmail.com>
2025-11-16 02:06:28 +0100

    Fixing solution for task 3

--- pipeline/apis/3-first_launch.py
+++ pipeline/apis/3-first_launch.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
-"""A script that  displays the first launch with some information"""
+"""A script that  displays the upcoming launch with some information"""
 
 
 import requests
 
 
-def first_launch():
-    """A function that displays the first launch with some information"""
+def upcoming_launch():
+    """A function that displays the upcoming launch with some information"""
 
     url = "https://api.spacexdata.com"
 
     launches = requests.get(f"{url}/v5/launches", timeout=10).json()
-    first = min(launches, key=lambda launch: launch["date_unix"])
 
-    launch_name = first["name"]
-    local_date = first["date_local"]
-    rocket_id = first["rocket"]
-    launchpad_id = first["launchpad"]
+    upcoming = [launch for launch in launches if launch["upcoming"]]
+    upcoming.sort(key=lambda launch: launch["date_unix"])
+
+    name = upcoming[0]["name"]
+    date = upcoming[0]["date_local"]
+    rocket_id = upcoming[0]["rocket"]
+    launchpad_id = upcoming[0]["launchpad"]
 
     rockets = requests.get(f"{url}/v4/rockets/{rocket_id}").json()
     rocket_name = rockets['name']
@@ -26,10 +28,10 @@ def first_launch():
     launchpad_locality = launchapds["locality"]
 
     return (
-        f"{launch_name} ({local_date}) {rocket_name} - "
+        f"{name} ({date}) {rocket_name} - "
         f"{launchpad_name} ({launchpad_locality})"
     )
 
 
 if __name__ == "__main__":
-    print(first_launch())
+    print(upcoming_launch())

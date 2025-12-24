35d0d51ff3ee6e42295d4e382d5ab2334cbc7bc0
saralozi <saralozi08@gmail.com>
2025-12-24 23:39:06 +0100

    Adding solution for task 1

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -6,43 +6,53 @@ that performs K-means on a dataset"""
 import numpy as np
 
 
+def initialize(X, k):
+    """Initialize cluster centroids for K-means"""
+
+    _, d = X.shape
+
+    centroids = np.random.uniform(low=np.min(
+        X, axis=0), high=np.max(X, axis=0), size=(k, d))
+
+    return centroids
+
+
 def kmeans(X, k, iterations=1000):
     """A function that performs K-Means on a dataset"""
 
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None
+
     if not isinstance(k, int) or k <= 0:
         return None, None
+
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
 
-    n, d = X.shape
-
-    min_vals = np.min(X, axis=0)
-    max_vals = np.max(X, axis=0)
-
-    C = np.random.uniform(low=min_vals, high=max_vals, size=(k, d))
-
-    pool = np.random.uniform(
-        low=min_vals, high=max_vals, size=(iterations * k, d))
-    pool_i = 0
+    C = initialize(X, k)
 
     for _ in range(iterations):
-        C_prev = C.copy()
+        C_prev = np.copy(C)
+        sum_cluster_points = np.zeros_like(C)
+        n_cluster_points = np.zeros((k, 1))
 
-        dist = ((X[:, None, :] - C[None, :, :]) ** 2).sum(axis=2)
-        clss = dist.argmin(axis=1)
+        distances = np.linalg.norm(X[:, np.newaxis, :] - C, axis=-1)
+        clss = np.argmin(distances, axis=1)
 
-        counts = np.bincount(clss, minlength=k)
-        sums = np.zeros((k, d))
-        np.add.at(sums, clss, X)
-        C = sums / np.where(counts[:, None] == 0, 1, counts[:, None])
+        for i in range(k):
+            cluster_points = X[clss == i]
+            if len(cluster_points) == 0:
+                C[i] = initialize(X, 1)[0]
+            else:
+                sum_cluster_points[i] = np.sum(cluster_points, axis=0)
+                n_cluster_points[i] = cluster_points.shape[0]
 
-        empty = counts == 0
-        if np.any(empty):
-            m = empty.sum()
-            C[empty] = pool[pool_i:pool_i + m]
-            pool_i += m
+        non_empty_clusters = n_cluster_points.flatten() != 0
+        C[non_empty_clusters] = sum_cluster_points[non_empty_clusters] / \
+            n_cluster_points[non_empty_clusters]
+
+        distances = np.linalg.norm(X[:, np.newaxis, :] - C, axis=-1)
+        clss = np.argmin(distances, axis=1)
 
         if np.array_equal(C, C_prev):
             break

335cab7adb48c9903136518b95d82d8597da017b
saralozi <saralozi08@gmail.com>
2025-12-25 16:09:57 +0100

    Adding solution for task 1

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -7,20 +7,12 @@ import numpy as np
 def pca(X, var=0.95):
     """Performs PCA on a dataset"""
 
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
-        return None
-    if not isinstance(var, float) or var <= 0 or var > 1:
-        return None
+    U, S, Vh = np.linalg.svd(X)
+    V = Vh.T
 
-    # X is already mean-centered (task guarantees mean = 0 per column)
-    _, s, Vt = np.linalg.svd(X, full_matrices=False)
+    cumsum = np.cumsum(S)
+    cumsum /= cumsum[-1]
 
-    # Explained variance is proportional to singular values squared
-    explained = s ** 2
-    cum_ratio = np.cumsum(explained) / np.sum(explained)
+    r = np.where(cumsum >= var)[0][0]
 
-    # smallest nd such that we keep at least `var`
-    nd = np.where(cum_ratio >= var)[0][0] + 1
-
-    W = Vt.T[:, :nd]
-    return W
+    return V[:, :r + 1]
--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -8,18 +8,8 @@ import numpy as np
 def pca(X, ndim):
     """A function that performs PCA on a dataset"""
 
-    if (not isinstance(X, np.ndarray) or X.ndim != 2 or
-            not isinstance(ndim, int) or ndim <= 0 or ndim > X.shape[1]):
-        return None
+    X_m = X - np.mean(X, axis=0)
+    U, S, Vh = np.linalg.svd(X_m)
+    W = Vh.T[:, :ndim]
 
-    X_centered = X - np.mean(X, axis=0)
-
-    cov = np.cov(X_centered, rowvar=False)
-
-    eigvals, eigvecs = np.linalg.eigh(cov)
-
-    idx = np.argsort(eigvals)[::-1]
-    W = eigvecs[:, idx[:ndim]]
-
-    T = X_centered @ W
-    return T
+    return X_m @ W

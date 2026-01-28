55504b5e4703773d74d9023cef0cd8a853f0b714
saralozi <saralozi08@gmail.com>
2026-01-28 16:08:10 +0100

    Adding solution for task 0

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -6,29 +6,27 @@ import tensorflow as tf
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """A function that builds a neural network with the Keras library"""
-
+    """Builds a neural network using the Keras Sequential API"""
     model = K.models.Sequential()
     reg = K.regularizers.l2(lambtha)
 
-    model.add(
-        K.layers.Dense(
-            units=layers[0],
-            activation=activations[0],
-            kernel_regularizer=reg,
-            input_shape=(nx,)
-        )
-    )
-    model.add(K.layers.Dropout(rate=1 - keep_prob))
+    model.add(K.layers.Dense(
+        units=layers[0],
+        activation=activations[0],
+        kernel_regularizer=reg,
+        input_shape=(nx,)
+    ))
+
+    if len(layers) > 1:
+        model.add(K.layers.Dropout(rate=1 - keep_prob))
 
     for i in range(1, len(layers)):
-        model.add(
-            K.layers.Dense(
-                units=layers[i],
-                activation=activations[i],
-                kernel_regularizer=reg
-            )
-        )
+        model.add(K.layers.Dense(
+            units=layers[i],
+            activation=activations[i],
+            kernel_regularizer=reg
+        ))
+
         if i != len(layers) - 1:
             model.add(K.layers.Dropout(rate=1 - keep_prob))
 

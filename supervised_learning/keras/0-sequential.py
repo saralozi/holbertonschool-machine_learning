a9b74785a2b54e10c0cadcfdd330cbc7077788e3
saralozi <saralozi08@gmail.com>
2026-01-28 15:47:41 +0100

    Adding solution for task 0

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""A function that builds a neural network with the Keras library"""
+
+import tensorflow.keras as K
+import tensorflow as tf
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """A function that builds a neural network with the Keras library"""
+
+    model = K.models.Sequential()
+    reg = K.regularizers.l2(lambtha)
+
+    # First layer
+    model.add(
+        K.layers.Dense(
+            units=layers[0],
+            activation=activations[0],
+            kernel_regularizer=reg,
+            input_shape=(nx,)
+        )
+    )
+    model.add(K.layers.Dropout(rate=1 - keep_prob))
+
+    # Remaining layers
+    for i in range(1, len(layers)):
+        model.add(
+            K.layers.Dense(
+                units=layers[i],
+                activation=activations[i],
+                kernel_regularizer=reg
+            )
+        )
+        if i != len(layers) - 1:
+            model.add(K.layers.Dropout(rate=1 - keep_prob))
+
+    return model

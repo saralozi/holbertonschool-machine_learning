8f533bf9bfbb8caedd8a00a840dad65aeb7299b8
saralozi <saralozi08@gmail.com>
2026-01-30 19:23:02 +0100

    Adding solution for task 8

---
+++ supervised_learning/keras/8-train.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""Trains a model using mini-batch gradient descent with optional validation,
+    early stopping, learning rate decay, and saving the best model"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                save_best=False, filepath=None,
+                verbose=True, shuffle=False):
+    """Trains a model using mini-batch gradient descent with optional
+      validation, early stopping, learning rate decay,
+        and saving the best model"""
+
+    callbacks = []
+    has_val = validation_data is not None
+
+    if early_stopping and has_val:
+        callbacks.append(
+            K.callbacks.EarlyStopping(
+                monitor='val_loss',
+                patience=patience
+            )
+        )
+
+    if learning_rate_decay and has_val:
+        def schedule(epoch):
+            return alpha / (1 + decay_rate * epoch)
+
+        callbacks.append(
+            K.callbacks.LearningRateScheduler(
+                schedule,
+                verbose=1
+            )
+        )
+
+    if save_best and has_val:
+        callbacks.append(
+            K.callbacks.ModelCheckpoint(
+                filepath=filepath,
+                monitor='val_loss',
+                save_best_only=True,
+                mode='min'
+            )
+        )
+
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle,
+        validation_data=validation_data,
+        callbacks=callbacks if callbacks else None
+    )
+
+    return history

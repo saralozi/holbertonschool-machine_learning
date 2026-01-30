84a4f526591f4056e3e3de93600115fc992ba391
saralozi <saralozi08@gmail.com>
2026-01-30 17:34:58 +0100

    Addong solution for task 1

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,33 +1,22 @@
 #!/usr/bin/env python3
 """A function that builds a neural network with the Keras library"""
 
-import tensorflow as tf
-from tensorflow import keras
-
+import tensorflow.keras as K
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """A function that builds a neural network with the Keras library"""
 
-    if len(layers) != len(activations):
-        raise ValueError("layers and activations must have the same length")
-
-    inputs = keras.Input(shape=(nx,))
+    inputs = K.Input(shape=(nx,))
 
     x = inputs
-
-    l2_reg = keras.regularizers.l2(lambtha)
-
     for i in range(len(layers)):
+        x = K.layers.Dense(layers[i],
+                           activation=activations[i],
+                           kernel_regularizer=K.regularizers.L2(lambtha))(x)
 
-        x = keras.layers.Dense(
-            units=layers[i],
-            activation=activations[i],
-            kernel_regularizer=l2_reg
-        )(x)
+        if i != len(layers) - 1 and keep_prob is not None:
+            x = K.layers.Dropout(1 - keep_prob)(x)
 
-        if i != len(layers) - 1:
-            x = keras.layers.Dropout(rate=1 - keep_prob)(x)
-
-    model = keras.Model(inputs=inputs, outputs=x)
+    model = K.Model(inputs, x)
 
     return model

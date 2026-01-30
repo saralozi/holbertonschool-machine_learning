853c973843f600ea91b37ba80aae80f2ecd410a7
saralozi <saralozi08@gmail.com>
2026-01-30 18:31:16 +0100

    Adding solution for task 4

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""A function that trains a model using mini-batch gradient descent"""
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
+    """A function that trains a model using mini-batch gradient descent"""
+
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+
+    return history

527e168e116530d95de4b48fccdc4c78d92d42a9
saralozi <saralozi08@gmail.com>
2026-01-30 19:46:34 +0100

    Adding solution for task 12

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -6,7 +6,6 @@ import tensorflow.keras as K
 
 def test_model(network, data, labels, verbose=True):
     """Tests a neural network model"""
-
     v = 1 if verbose else 0
     loss, acc = network.evaluate(data, labels, verbose=v)
-    return loss, acc
+    return [loss, acc]

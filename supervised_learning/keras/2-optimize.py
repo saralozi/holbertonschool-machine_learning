007401e9ce84d68c1975ec13bfad93ba3c965042
saralozi <saralozi08@gmail.com>
2026-01-30 18:13:14 +0100

    Adding solution for task 2

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python3
-"""Sets up Adam optimization for a keras model."""
+"""A function that sets up Adam optimization for a keras model
+with categorical crossentropy loss and accuracy metrics"""
 
 import tensorflow.keras as K
 
+
 def optimize_model(network, alpha, beta1, beta2):
-    """Sets up Adam optimization for a keras model."""
+    """A function that sets up Adam optimization for a keras model
+        with categorical crossentropy loss and accuracy metrics"""
+    Adam_optimizer = K.optimizers.Adam(learning_rate=alpha,
+                                       beta_1=beta1,
+                                       beta_2=beta2)
 
-    opt = tf.keras.optimizers.Adam(
-        learning_rate=alpha,
-        beta_1=beta1,
-        beta_2=beta2
-    )
-
-    if not hasattr(opt, "lr") and hasattr(opt, "learning_rate"):
-        opt.lr = opt.learning_rate
-
-    network.compile(
-        optimizer=opt,
-        loss="categorical_crossentropy",
-        metrics=["accuracy"]
-    )
+    network.compile(optimizer=Adam_optimizer,
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])

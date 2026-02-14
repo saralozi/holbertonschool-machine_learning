16608104bb657e30ae13f1ec6b2cc4a7d63a4482
saralozi <saralozi08@gmail.com>
2026-02-14 21:05:23 +0100

    Adding solution for task 3

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-"""A function that calculates the
- cost of a neural network with L2 regularization"""
+"""A function that calculates the cost
+ of a neural network with L2 regularization"""
 
 import tensorflow as tf
 
 
 def l2_reg_cost(cost, model):
-    """Calculate cost of neural network with L2 regularzation"""
+    """Calculates the total cost with L2 regularization"""
     layer_costs = []
 
     for layer in model.layers:
-        if layer.losses:
-            reg = tf.add_n(layer.losses)
-            layer_costs.append(cost + reg)
+        if len(layer.trainable_weights) == 0:
+            continue
+
+        reg = tf.add_n(layer.losses) if layer.losses else 0.0
+        layer_costs.append(cost + reg)
 
     return tf.stack(layer_costs)

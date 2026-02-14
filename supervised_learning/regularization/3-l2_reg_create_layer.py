29243001b3a600717faf7e70abbe49ffa23c0110
saralozi <saralozi08@gmail.com>
2026-02-14 21:31:52 +0100

    Adding solution for task 3

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
-"""A script that creates a layer with L2 regularization."""
+"""A function that creates a neural network layer with L2 regularization"""
 
 import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """
-    A function that creates a neural network layer with L2 regularization.
-    """
+    """A function that creates a neural network layer with L2 regularization"""
+
     initializer = tf.keras.initializers.VarianceScaling(
         scale=2.0,
         mode=('fan_avg')

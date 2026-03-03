5095d2e33c89376fc3b77492ac29f53095762a35
saralozi <saralozi08@gmail.com>
2026-03-03 01:59:08 +0100

    Adding solution for tsk 4

---
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Performs a convolution on images with channels"""
+
+import numpy as np
+
+
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """Performs a convolution on images with channels"""
+
+    m, h, w, c = images.shape
+    kh, kw, _ = kernel.shape
+    sh, sw = stride
+
+    if padding == 'valid':
+        ph, pw = 0, 0
+
+    elif padding == 'same':
+        ph = int((((h - 1) * sh + kh - h) / 2) + 1)
+        pw = int((((w - 1) * sw + kw - w) / 2) + 1)
+
+    elif isinstance(padding, tuple):
+        ph, pw = padding
+
+    output_height = int((h - kh + 2 * ph) / sh + 1)
+    output_width = int((w - kw + 2 * pw) / sw + 1)
+
+    convolved_images = np.zeros((m, output_height, output_width))
+
+    image_pad = np.pad(images,
+                       ((0, 0), (ph, ph),
+                        (pw, pw), (0, 0)), mode='constant')
+
+    for i in range(output_height):
+        for j in range(output_width):
+            image_zone = image_pad[:, i * sh:i * sh + kh,
+                                   j * sw:j * sw + kw, :]
+
+            convolved_images[:, i, j] = np.sum(image_zone * kernel,
+                                               axis=(1, 2, 3))
+
+    return convolved_images

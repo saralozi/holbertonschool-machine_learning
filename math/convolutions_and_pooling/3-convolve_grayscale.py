a50393b660c00304d66288e10121f1d5155ffa09
saralozi <saralozi08@gmail.com>
2026-03-03 01:56:25 +0100

    Adding solution for task 3

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -14,52 +14,29 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
 
     if padding == 'valid':
         ph, pw = 0, 0
-        ph_extra, pw_extra = 0, 0
 
     elif padding == 'same':
 
-        out_h = int(np.ceil(h / sh))
-        out_w = int(np.ceil(w / sw))
+        ph = int((((h - 1) * sh + kh - h)/2) + 1)
+        pw = int((((w - 1) * sw + kw - w)/2) + 1)
 
-        ph_total = max((out_h - 1) * sh + kh - h, 0)
-        pw_total = max((out_w - 1) * sw + kw - w, 0)
-
-        ph = ph_total // 2
-        pw = pw_total // 2
-
-        ph_extra = ph_total - 2 * ph
-        pw_extra = pw_total - 2 * pw
-
-    else:
+    elif isinstance(padding, tuple):
         ph, pw = padding
-        ph_extra, pw_extra = 0, 0
 
-    if padding == 'same':
-        padded = np.pad(
-            images,
-            pad_width=((0, 0), (ph, ph + ph_extra), (pw, pw + pw_extra)),
-            mode='constant',
-            constant_values=0
-        )
-    else:
-        padded = np.pad(
-            images,
-            pad_width=((0, 0), (ph, ph), (pw, pw)),
-            mode='constant',
-            constant_values=0
-        )
+    output_height = int((h - kh + 2 * ph) / sh + 1)
+    output_width = int((w - kw + 2 * pw) / sw + 1)
 
-    h_pad, w_pad = padded.shape[1], padded.shape[2]
-    out_h = ((h_pad - kh) // sh) + 1
-    out_w = ((w_pad - kw) // sw) + 1
+    convolved_images = np.zeros((m, output_height, output_width))
 
-    output = np.zeros((m, out_h, out_w))
+    image_pad = np.pad(images,
+                       ((0, 0), (ph, ph),
+                        (pw, pw)), mode='constant')
 
-    for i in range(out_h):
-        for j in range(out_w):
-            y = i * sh
-            x = j * sw
-            patch = padded[:, y:y + kh, x:x + kw]
-            output[:, i, j] = np.sum(patch * kernel, axis=(1, 2))
+    for i in range(output_height):
+        for j in range(output_width):
+            image_zone = image_pad[:, i * sh:i * sh + kh, j * sw:j * sw + kw]
 
-    return output
+            convolved_images[:, i, j] = np.sum(image_zone * kernel,
+                                               axis=(1, 2))
+
+    return convolved_images

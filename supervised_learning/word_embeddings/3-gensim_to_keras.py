316bffe52e4b137b6982c2de26f6e0a3667cfffe
saralozi <saralozi08@gmail.com>
2026-04-30 00:31:16 +0200

    Adding solutions

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Convert Gensim Word2Vec model to Keras Embedding layer"""
 
-from keras.layers import Embedding
+import tensorflow as tf
 
 
 def gensim_to_keras(model):
@@ -9,10 +9,10 @@ def gensim_to_keras(model):
 
     weights = model.wv.vectors
 
-    embedding = Embedding(
+    embedding = tf.keras.layers.Embedding(
         input_dim=weights.shape[0],
         output_dim=weights.shape[1],
-        weights=[weights],
+        embeddings_initializer=tf.keras.initializers.Constant(weights),
         trainable=True
     )
 

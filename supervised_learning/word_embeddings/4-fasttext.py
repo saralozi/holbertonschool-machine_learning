9acf3987d74a1c8f3b59b09c25358495898d7ece
saralozi <saralozi08@gmail.com>
2026-04-30 00:13:55 +0200

    Adding solutions for tasks 0-4

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 """Train FastText model"""
 
-from gensim.models import FastText
+import gensim
 
 
 def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
                    window=5, cbow=True, epochs=5, seed=0, workers=1):
     """Create, build and train a gensim FastText model"""
 
-    model = FastText(
+    model = gensim.models.FastText(
         vector_size=vector_size,
         min_count=min_count,
         negative=negative,
         window=window,
-        sg=int(not cbow),
+        sg=0 if cbow else 1,
         seed=seed,
         workers=workers
     )
